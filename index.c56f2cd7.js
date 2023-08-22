1842fc2a1be7efee44a9115f1c68632d1277dac6
IuliaPopoaca <134948676+IuliaPopoaca@users.noreply.github.com>
2023-08-22 17:20:39 +0000

    Deploying to gh-pages from @ IuliaPopoaca/wildcats@d473749e0402a569ba0cce81470409686ce8931b 🚀

---
+++ index.c56f2cd7.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{direction:"vertical",loop:!0,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{el:".swiper-pagination",type:"custom",renderCustom:function(e,n,t){return"0"+n+'<span class="line"></span>0'+t}},effect:"cube"});
+//# sourceMappingURL=index.c56f2cd7.js.map
